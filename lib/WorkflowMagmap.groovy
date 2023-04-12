c2653ee834b8bac2561b9f7c06ba1d27a76debbb
Daniel Lundin <erik.rikard.daniel@gmail.com>
2023-04-12 15:51:10 +0200

    Removed template params we don't need

--- lib/WorkflowMagmap.groovy
+++ lib/WorkflowMagmap.groovy
@@ -11,12 +11,6 @@ class WorkflowMagmap {
     //
     public static void initialise(params, log) {
         genomeExistsError(params, log)
-
-
-        if (!params.fasta) {
-            log.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
-            System.exit(1)
-        }
     }
 
     //
@@ -61,17 +55,10 @@ class WorkflowMagmap {
         def description_html = engine.createTemplate(methods_text).make(meta)
 
         return description_html
-    }//
+    }
+    //
     // Exit pipeline if incorrect --genome key provided
     //
     private static void genomeExistsError(params, log) {
-        if (params.genomes && params.genome && !params.genomes.containsKey(params.genome)) {
-            log.error "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
-                "  Genome '${params.genome}' not found in any config files provided to the pipeline.\n" +
-                "  Currently, the available genome keys are:\n" +
-                "  ${params.genomes.keySet().join(", ")}\n" +
-                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-            System.exit(1)
-        }
     }
 }
